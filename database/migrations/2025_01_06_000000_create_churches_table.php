8062c93de69c2e3a6623a211279bd1a4a4f685fc
benjaminzavala <benjamin@eadventist.net>
2026-01-09 10:21:49 -0500

    fix 2

--- database/migrations/2025_01_06_000000_create_churches_table.php
+++ database/migrations/2025_01_06_000000_create_churches_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('churches')) {
+            return;
+        }
+
         Schema::create('churches', function (Blueprint $table) {
             $table->id();
             $table->string('name');
